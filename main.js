f449630b07ae33845318b76bfadbb7b5f2c7c04d
aldairdiaz <irving.s21diaz@gmail.com>
2023-03-07 19:20:52 -0600

    "Define print method"

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-const prompt = require('prompt-sync')({sigint: true});
+'use strict';
+
+// const prompt = require('prompt-sync')({sigint: true});
 
 const hat = '^';
 const hole = 'O';
@@ -6,5 +8,24 @@ const fieldCharacter = '░';
 const pathCharacter = '*';
 
 class Field {
+    constructor(field) {
+        this._fieldd = field;
+    }
 
+    print() {
+        console.log('\n');
+        for (let i = 0; i < this._fieldd.length; i++) {
+            let row = this._fieldd[i];
+            console.log(`${row[0]}  ${row[1]}  ${row[2]}`);
+        }
+    }
 }
+
+
+const newField = new Field([
+    [pathCharacter, fieldCharacter, hole],
+    [fieldCharacter, hole, fieldCharacter],
+    [fieldCharacter, hat, fieldCharacter]
+]);
+
+newField.print()
