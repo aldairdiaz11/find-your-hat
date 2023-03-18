e49ffcba67df4c46e86c5dcb8d3c1a7dc95061ee
aldairdiaz <irving.s21diaz@gmail.com>
2023-03-18 10:26:04 -0600

    "Implement generate field"

--- main.js
+++ main.js
@@ -19,6 +19,34 @@ class Field {
             console.log(`${row[0]}  ${row[1]}  ${row[2]}`);
         }
     }
+
+    static generateField(height, width) {
+        // const options = [fieldCharacter, hole, fieldCharacter] // probability of 1/3 to get a hole
+        let field = [];
+        for (let h = 0; h < height; h++) {
+            let newRow = []
+            for (let w = 0; w < width; w++) {
+                newRow.push(options[Math.floor(Math.random() * options.length)]);
+            }
+            field.push(newRow);
+        }
+        field[0][0] = pathCharacter; // player start place
+
+        // Place hat in field
+        let posHat = false;
+        while (!posHat) {
+            const heightHat = Math.floor(Math.random() * height);
+            const widthHat = Math.floor(Math.random() * width);
+
+
+            if (widthHat && heightHat !== 0) {
+                console.log(heightHat, widthHat)
+                field[heightHat][widthHat] = hat;
+                posHat = true
+            }
+        }
+        return field;
+    }
 }
 
 
@@ -29,3 +57,15 @@ const newField = new Field([
 ]);
 
 newField.print()
+
+console.log(Field.generateField(4, 5))
+
+let game = true
+while (game) {
+    // todo ask the user to input next move until they'd find their hat
+
+    // todo after make a move, the game should print the current field map with the tiles they entered
+
+    // todo if the user wins, lose or
+    game = false
+}
