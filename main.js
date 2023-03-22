cd9cb3f88f982e86567ee375b6458907af540555
aldairdiaz <irving.s21diaz@gmail.com>
2023-03-21 22:32:50 -0600

    "Add user inputs and logic to the game"

--- main.js
+++ main.js
@@ -9,18 +9,30 @@ const pathCharacter = '*';
 
 class Field {
     constructor(h, w) {
-        this._field = Field.generateField(h, w)
+        this._hat_pos = [0, 0];
+        this._x_pos = 0;
+        this._y_pos = 0;
+        this._field = this.generateField(h, w);
+    }
+
+    set field(char) {
+        this._field[this._y_pos][this._x_pos] = char;
+    }
+
+    set hat_position(pos) {
+        this._hat_pos = pos;
     }
 
     print() {
         console.log('\n');
+        this.field = pathCharacter;
         for (let column = 0; column < this._field.length; column++) {
             let row = this._field[column];
             console.log(row.join(" "));
         }
     }
 
-    static generateField(height, width) {
+    generateField(height, width) {
         const options = [fieldCharacter, hole, fieldCharacter] // probability of 1/3 to get a hole
         let field = [];
         for (let h = 0; h < height; h++) {
@@ -30,7 +42,6 @@ class Field {
             }
             field.push(newRow);
         }
-        field[0][0] = pathCharacter; // player start place
 
         // Place hat in field
         let posHat = false;
@@ -38,26 +49,77 @@ class Field {
             const heightHat = Math.floor(Math.random() * height);
             const widthHat = Math.floor(Math.random() * width);
 
-
             if (widthHat && heightHat !== 0) {
                 field[heightHat][widthHat] = hat;
-                posHat = true
+                this.hat_position = [heightHat, widthHat];
+                posHat = true;
             }
         }
         return field;
     }
+
+    move_player(direction) {
+        const valid_moves = ["a", "w", "s", "d"];
+
+        // Check if user input a valid direction
+
+        if (valid_moves.includes(direction)) {
+            switch (direction) {
+                case "a":
+                    this._x_pos -= 1;
+                    break;
+                case "w":
+                    this._y_pos += 1;
+                    break;
+                case "s":
+                    this._y_pos -= 1;
+                    break;
+                case "d":
+                    this._x_pos += 1;
+                    break;
+                default:
+                    break;
+            }
+            return true;
+        } else {
+            return false;
+        }
+        // if not skip
+    }
+
+    check_game_status() {
+        // Winning case:
+        if (this._hat_pos[0] === this._y_pos && this._hat_pos[1] === this._x_pos) {
+            console.log("You win");
+            return true;
+
+            // Out of bones options: // todo work on this
+        } else if (this._y_pos > this.field.length) {
+            return false;
+        }
+    }
+
 }
 
 
 const newField = new Field(5, 6);
 newField.print()
 
+const prompt = require('prompt-sync')({sigint: true});
+
 let game = true
 while (game) {
     // todo ask the user to input next move until they'd find their hat
+    console.log("Make a move: ⬅️(a)⬆️(w)➡️(d)⬇️(s)");
+
+
+    let input = prompt(''); // Use node to ask the user to input a direction
 
     // todo after make a move, the game should print the current field map with the tiles they entered
-
-    // todo if the user wins, lose or
-    game = false
+    if (newField.move_player(input)) {
+        // todo if the user wins or lose end the game
+        if (newField.check_game_status())
+            game = false;
+        newField.print();
+    }
 }
